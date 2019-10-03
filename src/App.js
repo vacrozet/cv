79deaa49de610557ecbbec5995488ef89632edf5
valentin <crozet.valentin.42@gmail.com>
2019-10-03 14:17:53 +0200

    new

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+import { Eclipse } from "react-loading-io";
+
+const Index = () => {
+  return <Eclipse size={120} />;
+};
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>En cours de création...</p>
+        <Index />
+      </header>
+    </div>
+  );
+}
+
+export default App;
