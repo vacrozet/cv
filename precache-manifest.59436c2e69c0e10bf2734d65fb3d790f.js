b0f2a2e0e64435526a8fa88a8108675de7899fd7
valentin <crozet.valentin.42@gmail.com>
2019-10-03 14:19:20 +0200

    Updates

---
+++ precache-manifest.59436c2e69c0e10bf2734d65fb3d790f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1893f4b11cd6f0ba2fd43184849f5fdb",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "312f22fb9313e4cf8629",
+    "url": "/cv/static/css/2.772b860c.chunk.css"
+  },
+  {
+    "revision": "2ba70de87ccfc29eb593",
+    "url": "/cv/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "312f22fb9313e4cf8629",
+    "url": "/cv/static/js/2.19b23443.chunk.js"
+  },
+  {
+    "revision": "2ba70de87ccfc29eb593",
+    "url": "/cv/static/js/main.0096eb36.chunk.js"
+  },
+  {
+    "revision": "15c1fbe4e3f13ef574e9",
+    "url": "/cv/static/js/runtime-main.3131271d.js"
+  }
+]);
