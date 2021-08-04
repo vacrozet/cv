f6b233822a98cd9d334db04a7d8187c16befdd69
valentin crozet <crozet.valentin.42@gmail.com>
2021-08-04 11:27:57 +0200

    button download

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { Eclipse } from "react-loading-io";
+import FileSaver from "file-saver";
 
-const Index = () => {
-  return <Eclipse size={120} />;
+const importAll = (r) => {
+  return r.keys().map(r);
 };
 
-function App() {
+const findFile = () => {
+  const images = importAll(require.context("./", false, /\.(pdf)$/));
+  if (images.length === 1) {
+    return images[0];
+  }
+};
+
+const App = () => {
+  const [url, setUrl] = useState("");
+
+  const saveFile = () => {
+    FileSaver.saveAs(url, "cv_valentin.pdf");
+  };
+
+  useEffect(() => {
+    const image = findFile();
+    setUrl(image);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <p>En cours de rénovation...</p>
-        <Index />
+        <button
+          onClick={() => {
+            saveFile();
+          }}
+        >
+          Télécharger
+        </button>
+        <Eclipse size={120} />
       </header>
     </div>
   );
-}
+};
 
 export default App;
